16e3c3b69e912bb3bf56b851c9150dd78791b0cd
Steven Tappert <admin@dark-it.net>
2015-06-28 22:14:18 +0200

    boilerplate

---
+++ assets/js/walkingleftgame.js
@@ -0,0 +1,19 @@
+WalkingLeftGame = function(game) {
+    this.game = game;
+};
+
+WalkingLeftGame.prototype.preload = function() {
+
+};
+
+WalkingLeftGame.prototype.create = function() {
+
+};
+
+WalkingLeftGame.prototype.render = function() {
+
+};
+
+WalkingLeftGame.prototype.update = function () {
+
+};
